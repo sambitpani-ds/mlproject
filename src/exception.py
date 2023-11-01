0018d555e70ef5c1f96ddf5e287b5a683c7eda43
Sambit <sambitpani2@gmail.com>
2023-11-02 00:26:51 +0530

    EDA and ML checking

--- src/exception.py
+++ src/exception.py
@@ -5,7 +5,7 @@ def error_message_detail(error,error_detail:sys):
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
-    return error_message
+        return error_message
     )
 class CustomeException(Exception):
     def __init__(self,error_message,error_detail:sys):
