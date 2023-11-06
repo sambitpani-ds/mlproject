53fafc4014e5772b3e224272e6f27cacbba9a216
Sambit <sambitpani2@gmail.com>
2023-11-06 21:34:15 +0530

    modeling and best model finding.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -45,8 +47,13 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__ == "__main__":
-    obj = DataIngestion()
+    obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-    
+
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    
+    
